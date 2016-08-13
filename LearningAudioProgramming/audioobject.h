3110510b844d628d602b9ed539f32e00966bde67
ajanaliz <jchbbst@gmail.com>
2016-08-14 00:06:45 +0430

    setting up audio architecture

---
+++ LearningAudioProgramming/audioobject.h
@@ -0,0 +1,23 @@
+#ifndef AUDIO_OBJECT_INCLUDED_HPP
+#define AUDIO_OBJECT_INCLUDED_HPP
+
+#include "iaudiodata.h"
+#include "sampleinfo.h"
+
+class AudioObject
+{
+public:
+	AudioObject(const SampleInfo& info, IAudioData* data);
+
+	bool GenerateSamples(float* stream, size_t streamLength);
+	void SetPos(double pos);
+private:
+	size_t      m_audioPos;
+	size_t      m_audioLength;
+	SampleInfo  m_sampleInfo;
+	IAudioData* m_audioData;
+
+	size_t PosToAbsolutePos(double pos);
+};
+
+#endif
